6c023f843d5b78d278fda88e78c47d466f5c4eee
marcinfojucik <72644783+marcinfojucik@users.noreply.github.com>
2021-12-30 10:03:41 +0100

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,12 @@
+// Program wykonujący proste obliczenia
+class Example2 {
+public static void main(String args[]) {
+    int var1; //deklaracja zmiennej
+    int var2; //deklaracja drugiej zmiennej
+    var1 = 4; //przypisanie 4 do var1
+    System.out.println("var1 zawiera " + var1);
+    var2 = var1 / 2;
+    System.out.print("var2 zawiera var1 / 2: ");
+    System.out.println(var2);
+}
+}
