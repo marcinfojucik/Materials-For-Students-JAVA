fb37147e17cbde6d6739bb46c15d6a0118d91239
marcinfojucik <72644783+marcinfojucik@users.noreply.github.com>
2021-12-30 10:06:28 +0100

    Add files via upload

---
+++ For.java
@@ -0,0 +1,10 @@
+// Przykład Pętli For
+class For {
+    public static void main (String args[]) {
+        int x;
+        for(x = 0; x < 10;) {
+            System.out.println("Przebieg " + x);
+            x++; // inkrementuje zmienną sterującą pętli 
+        }
+    }
+}
