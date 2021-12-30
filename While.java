5af7a73ba84ffdbbe51b8abefc796282f69dc808
marcinfojucik <72644783+marcinfojucik@users.noreply.github.com>
2021-12-30 10:06:52 +0100

    Add files via upload

---
+++ While.java
@@ -0,0 +1,12 @@
+// Przykład Pętli While
+class While {
+    public static void main(String args[]) {
+        char x;
+        //wyświetla alfbet za pomocą pętli while
+        x = 'a';
+        while(x <= 'z') {
+            System.out.print(x);
+            x++;
+        }
+    }
+}
